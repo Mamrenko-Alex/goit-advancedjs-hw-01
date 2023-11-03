9d58a068f97c5474ff494dd171a93b3d278aae78
Mamrenko-Alex <Mamrenkodev@yandex.com>
2023-11-03 20:17:07 +0100

    Vimeo player add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player')
+
+const iframePlayer = new Player(iframe);
+
+// Функція для збереження поточного часу відтворення у локальне сховище
+function saveCurrentTime(time) {
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(time));
+}
+
+// Функція для відновлення часу відтворення зі збереженої позиції
+function restoreCurrentTime() {
+    const currentTime = localStorage.getItem("videoplayer-current-time");
+    iframePlayer.setCurrentTime(Number(JSON.parse(currentTime)) || 0);
+}
+
+// Відстеження події timeupdate і збереження часу відтворення з певною затримкою
+iframePlayer.on('timeupdate', throttle(event => {
+  saveCurrentTime(event.seconds);
+}, 1000));
+
+// Відновлення часу відтворення під час перезавантаження сторінки
+restoreCurrentTime();
